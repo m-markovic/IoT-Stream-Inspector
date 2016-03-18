7b024e84a4679337c59b56ba93276396836af600
Niels H. Christensen <nhc@mayacs.com>
2016-03-18 09:54:53 +0000

    Prepare ExampleMain for live and recorded data examples

--- src/main/java/uk/ac/abdn/iotstreams/ExampleMain.java
+++ src/main/java/uk/ac/abdn/iotstreams/ExampleMain.java
@@ -1,16 +1,31 @@
 package uk.ac.abdn.iotstreams;
 
+import eu.larkc.csparql.common.config.Config;
+
 /**
- * 
  * @author nhc
  *
+ * Examples of how to use this project.
+ * Note that the file "csparql.properties" in the project root
+ * has a setting that determines whether live data streams
+ * will be used. See README.md for details.
  */
 public final class ExampleMain {
     /**
+     * Main method with examples of how to use this project.
      * @param args not used
      */
     public static void main(final String[] args) {
-        //TODO
+        //Check csparql.properties to determine whether live or recorded data will be used
+        if (useRecordedData()) {
+            //TODO
+        } else {// Live data stream
+            //TODO
+        }
+    }
+    
+    private static boolean useRecordedData() {
+        return Config.INSTANCE.isEsperUsingExternalTimestamp();
     }
     
 }
